d82aa56d43f7550328783feee2f497fc62986c8e
P-BUS <pavlo.buslowski@gmail.com>
2024-01-21 00:35:03 +0200

    instrumentation test added

--- app/src/androidTest/java/com.example.tiptime/TipUITests.kt
+++ app/src/androidTest/java/com.example.tiptime/TipUITests.kt
@@ -1,8 +1,12 @@
 package com.example.tiptime
 
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
+import com.example.tiptime.ui.theme.TipTimeTheme
 import org.junit.Rule
 import org.junit.Test
+import java.text.NumberFormat
 
 class TipUITests {
     @get:Rule
@@ -10,6 +14,18 @@ class TipUITests {
 
     @Test
     fun calculate_20_percent_tip() {
+        composeTestRule.setContent {
+            TipTimeTheme {
+                TipTimeLayout()
+            }
+        }
+        // prepare
+        composeTestRule.onNodeWithText("Bill Amount").performTextInput("10")
+        composeTestRule.onNodeWithText("Tip Percentage").performTextInput("20")
+        // check
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        composeTestRule.onNodeWithText("Tip Amount: $expectedTip")
+            .assertExists("No node with this text was found.")
 
     }
 }
